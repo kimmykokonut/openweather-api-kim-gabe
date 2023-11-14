361e31057e73476bd9ccf0755f1fddb01d6dd846
Kim Robinson <kimmykokonut@gmail.com>
2023-11-14 11:30:07 -0800

    add input for API request for air quality by zip and return lat-long aqi
    Co-authored-by: Gabriel Tucker <gabrieltucker260@gmail.com>

--- src/index.js
+++ src/index.js
@@ -32,7 +32,42 @@ function getWeather(city) {
   request.send();
 }
 
+function getLLFromZip(zip) { 
+  let request = new XMLHttpRequest();
+  const urlZipToLL = `http://api.openweathermap.org/geo/1.0/zip?zip=${zip},US&appid=${process.env.API_KEY}`;
+
+  request.addEventListener("loadend", function() {
+    let response = JSON.parse(this.responseText);
+    let latInput = response.lat;
+    let longInput = response.lon;
+
+    if (this.status === 200) {
+      getAirQuality(latInput, longInput);
+    }
+  });
+  request.open("GET", urlZipToLL, true);
+  request.send();
+}
+
+function getAirQuality(latInput, longInput) {
+  let request = new XMLHttpRequest();
+  const url = `https://api.openweathermap.org/data/2.5/air_pollution?lat=${latInput}&lon=${longInput}&appid=${process.env.API_KEY}`;
+
+  request.addEventListener("loadend", function() {
+    let response = JSON.parse(this.responseText);
+    if (this.status === 200) {
+      printElementsAQ(response, latInput, longInput);
+    }
+  });
+
+  request.open("GET", url, true);
+  request.send();
+}
+
 // UI Logic
+function printElementsAQ(apiResponse, latInput, longInput) {
+  document.querySelector('#showAirQuality').innerText = `The Air Quality Index at latitude ${latInput} and longitude ${longInput} is ${apiResponse.list[0].main.aqi}!`;
+}
 
 function printElements(apiResponse, location) {
   const tempF = Math.round(((apiResponse.main.temp - 273.15) * 9/5 + 32) * 100) / 100;
@@ -55,7 +90,15 @@ function handleZipFormSubmission(event) {
   getWeatherZ(zip);
 }
 
+function handleAQSubmission(e) {
+  e.preventDefault();
+  const zipAQ = document.querySelector('#airQZ').value;
+  document.querySelector('#airQZ').value = null;
+  getLLFromZip(zipAQ);
+}
+
 window.addEventListener("load", function() {
   document.querySelector('form#city').addEventListener("submit", handleFormSubmission);
   document.querySelector('form#zip').addEventListener("submit", handleZipFormSubmission);
+  document.querySelector('form#airQ').addEventListener("submit", handleAQSubmission);
 });
