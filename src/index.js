917fb80d75010960ab18a16106c94659aecea83e
Student <student@epicodus-7.hsd1.or.comcast.net>
2023-11-14 09:03:28 -0800

    basic application started for project

--- src/index.js
+++ src/index.js
@@ -1,21 +1,38 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Triangle from './js/triangle.js';
 
-function handleTriangleForm(e) {
-  e.preventDefault();
-  document.querySelector('#response').innerText = null;
-  const length2 = parseInt(document.querySelector('#length2').value);
-  const length1 = parseInt(document.querySelector('#length1').value);
-  const length3 = parseInt(document.querySelector('#length3').value);
-  const triangle = new Triangle(length1, length2, length3);
-  const response = triangle.checkType();
-  const pTag = document.createElement("p");
-  pTag.append(response);
-  document.querySelector('#response').append(pTag);
+// Business Logic
+
+function getWeather(city) {
+  let request = new XMLHttpRequest();
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+  
+  request.addEventListener("loadend", function() {
+    const response = JSON.parse(this.responseText);
+    if (this.status === 200) {
+      printElements(response, city);
+    }
+  });
+
+  request.open("GET", url, true);
+  request.send();
 }
 
-window.addEventListener("load", function () {
-  document.querySelector("#triangle-checker-form").addEventListener("submit", handleTriangleForm);
+// UI Logic
+
+function printElements(apiResponse, city) {
+  document.querySelector('#showResponse').innerText = `The humidity in ${city} is ${apiResponse.main.humidity}%.
+  The temperature in Kelvins is ${apiResponse.main.temp} degrees.`;
+}
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const city = document.querySelector('#location').value;
+  document.querySelector('#location').value = null;
+  getWeather(city);
+}
+
+window.addEventListener("load", function() {
+  document.querySelector('form').addEventListener("submit", handleFormSubmission);
 });
